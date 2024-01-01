9f2a4ec5bb44e68c4dc55c3a7de1b0f5d7f5ae71
valentinenyongesa <valentinenyongesa94@gmail.com>
2024-01-01 02:50:10 -0800

    starts a Flask web application

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -4,15 +4,18 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """Displays 'Hello HBNB!' when accessing the root route."""
     return "Hello HBNB!"
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """Displays 'HBNB' when accessing the /hbnb route."""
     return "HBNB"
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
